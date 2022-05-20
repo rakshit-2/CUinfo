2d9fdd59516897e9152a4540d8c1e1d4ac978600
rakshit4013 <rakshit1063@chitkara.edu.in>
2022-05-20 17:42:37 +0530

    server made full

--- server/index.js
+++ server/index.js
@@ -6,8 +6,25 @@ const cors=require('cors');
 // const { spawn } = require('child_process');
 
 
+const db=mysql.createPool({
+    host:"localhost",
+    user:"root",
+    password:"password",
+    database:"CUinfo",
+});
+
+app.use(cors());
+app.use(express.json())
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
+
+
+
+
 app.listen(3001,()=>{
-    console.log("running 3001")
+    console.log("CUinfo server running 3001")
 });
 
 
