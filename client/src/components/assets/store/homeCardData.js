6d13d5b88bbb83472795f47e0940a6a8428b7aed
rakshit4013 <rakshit1063@chitkara.edu.in>
2022-05-23 19:04:30 +0530

    store added fixed card each code

---
+++ client/src/components/assets/store/homeCardData.js
@@ -0,0 +1,28 @@
+import notes_image from './../../assets/images/notes logo.png';
+import dsa_img from './../../assets/images/dsa logo.png';
+import event_logo from './../../assets/images/event logo.png';
+import youtube_logo from './../../assets/images/youtube logo.png';
+
+const homeCardData = {
+    0:  {
+        name:"Notes",
+        image:notes_image,
+        info:"Subject notes, handwritten 100% cho covered Subject notes, handwritten 100% cho covered",
+        },
+    1:  {
+        name:"DSA",
+        image:dsa_img,
+        info:"Subject notes, handwritten 100% cho covered Subject notes, handwritten 100% cho covered",
+        },
+    2:  {
+        name:"Event/Notice",
+        image:event_logo,
+        info:"Subject notes, handwritten 100% cho covered Subject notes, handwritten 100% cho covered",
+        },
+    3:  {
+        name:"Youtube",
+        image:youtube_logo,
+        info:"Subject notes, handwritten 100% cho covered Subject notes, handwritten 100% cho covered",
+        },
+    };
+  export default homeCardData;
