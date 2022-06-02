60278bd88cc02d085c4c585f195333ba75a6fb81
rakshit4013 <rakshit1063@chitkara.edu.in>
2022-06-02 14:18:06 +0530

    dsa section created not responsive

--- client/src/components/organism/dsa/index.js
+++ client/src/components/organism/dsa/index.js
@@ -1,10 +1,32 @@
 import './index.css';
+import dsa_img1 from './../../assets/images/dsaimage/dsa img10.svg';
+import DsaSectionDisplay from './../../molecule/dsaSectionDisplay/index';
 
 
+function sectionDisplay()
+{
+  
+  var li=[];
+  for(var i=0;i<4;i++)
+  {
+    li.push(<DsaSectionDisplay id={i}/>)
+  }
+  return li;
+}
 const  Dsa=()=> {
   return (
     <>
-    dsa
+      <div className="dsa__outer__outer" >
+        <div className="dsa__outer">
+          <div className="dsa__section1">
+            <img src={dsa_img1} className="dsa_img" alt=""/>
+          </div>
+          <div className="dsa__heading">
+            Data Structure And Algorithms (DSA)
+          </div>
+          {sectionDisplay()}
+        </div>
+      </div>
     </>
   );
 }
--- client/src/components/organism/notes/index.js
+++ client/src/components/organism/notes/index.js
@@ -1,46 +1,46 @@
-import React, { useState } from "react";
+import React from "react";
 import './index.css';
 import items from "./../../assets/store/notesCardData";
 import notes_img1 from './../../assets/images/notesImage/notes img1.svg'
-import CatogaryFile from './../../atoms/notesCatogaryEach/index';
+// import CatogaryFile from './../../atoms/notesCatogaryEach/index';
 import Card from './../../atoms/notesCardEach/index';
 
-const allCategories = ["all", ...new Set(items.map((item) => item.category))];
+// const allCategories = ["all", ...new Set(items.map((item) => item.category))];
 
 const Notes=()=> {
   
-  const [cardItems, setCardItems] = useState(items);
-  const [activeCategory, setActiveCategory] = useState("");
-  const [categories, setCategories] = useState(allCategories);
-  const [notesDisplay, setNotesDisplay] = useState({page:"block",filter:"none"});
+  // const [cardItems, setCardItems] = useState(items);
+  // const [activeCategory, setActiveCategory] = useState("");
+  // const [categories, setCategories] = useState(allCategories);
+  // const [notesDisplay, setNotesDisplay] = useState({page:"block",filter:"none"});
 
-  const changeFilterDisplayFunc=(x)=>{
-    if(x===1)
-    {
-      setNotesDisplay({page:"none",filter:"block"});
-    }
-    else
-    {
-      setNotesDisplay({page:"block",filter:"none"});
-    }
+  // const changeFilterDisplayFunc=(x)=>{
+  //   if(x===1)
+  //   {
+  //     setNotesDisplay({page:"none",filter:"block"});
+  //   }
+  //   else
+  //   {
+  //     setNotesDisplay({page:"block",filter:"none"});
+  //   }
     
-  }
+  // }
   
-  const filterItems = (x) => {
-    changeFilterDisplayFunc(0);
-    setActiveCategory(x);
-    if (x==="all") {
-      setCardItems(items);
-      // return;
-    }
-    const newItems = items.filter((item) => item.category === x);
-    setCardItems(newItems);
-  };
+  // const filterItems = (x) => {
+  //   changeFilterDisplayFunc(0);
+  //   setActiveCategory(x);
+  //   if (x==="all") {
+  //     setCardItems(items);
+  //     // return;
+  //   }
+  //   const newItems = items.filter((item) => item.category === x);
+  //   setCardItems(newItems);
+  // };
 
 
   return (
     <>
-    <div className="notes__outer__outer" style={{display:notesDisplay.page}}>
+    <div className="notes__outer__outer" >
       <div className="notes__outer">
         <div className="notes__section1">
           <img src={notes_img1} className="notes_img" alt=""/>
@@ -57,7 +57,7 @@ const Notes=()=> {
           />
         </div> */}
         <div className="notes__card__outer">
-          <Card items={cardItems} />
+          <Card items={items} />
         </div>
       </div>
     </div>
