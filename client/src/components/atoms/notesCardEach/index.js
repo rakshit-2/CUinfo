f0291d027837d0c51737251aaa4d48181749194f
rakshit4013 <rakshit1063@chitkara.edu.in>
2022-06-27 16:47:04 +0530

    view pdf button added

--- client/src/components/atoms/notesCardEach/index.js
+++ client/src/components/atoms/notesCardEach/index.js
@@ -1,6 +1,15 @@
 import React, {useState } from "react";
 import './index.css';
 import download_icon from './../../assets/icon/download.svg';
+import pdf1 from './../../assets/pdfs/pdf_subid_1.pdf';
+import pdf2 from './../../assets/pdfs/pdf_subid_2.pdf';
+import pdf3 from './../../assets/pdfs/pdf_subid_3.pdf';
+import pdf4 from './../../assets/pdfs/pdf_subid_4.pdf';
+import pdf5 from './../../assets/pdfs/pdf_subid_5.pdf';
+import pdf6 from './../../assets/pdfs/pdf_subid_6.pdf';
+import pdf7 from './../../assets/pdfs/pdf_subid_7.pdf';
+import pdf8 from './../../assets/pdfs/pdf_subid_8.pdf';
+import pdf9 from './../../assets/pdfs/pdf_subid_9.pdf';
 
 
 
@@ -9,10 +18,16 @@ const notesCardEach = ({ items }) => {
     <div className="notes__card__section">
       {items.map((item) => {
         const [val,setVal]=useState("");
+        const [view,setView]=useState();
         const { id, title, img, desc } = item;
         function handlePdf(x)
         {
           setVal("http://localhost:3001/pdf"+String(x));
+        }
+        function handlePdfView(x)
+        {
+          var li=[pdf1,pdf2,pdf3,pdf4,pdf5,pdf6,pdf7,pdf8,pdf9];
+          setView(li[x-1]);
         }
         return (
             <div key={id} className="notes__card__item">
@@ -25,6 +40,9 @@ const notesCardEach = ({ items }) => {
                 <div className="notes__card__button">
                   <a href={val} className="notes__card__download__button" onClick={()=>{handlePdf(id)}}>
                     <img src={download_icon} className="notes__download" alt=""/>{'\u00A0'}Download PDF
+                  </a>
+                  <a href={view} target="_black" className="notes__card__download__button" onClick={()=>{handlePdfView(id)}}>
+                    <img src={download_icon} className="notes__download" alt=""/>{'\u00A0'}View PDF
                   </a>
                 </div>
               </div>
