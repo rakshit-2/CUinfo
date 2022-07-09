c827aa0948c37f9941751324bdc2e84ccb99ce69
rakshit4013 <rakshit1063@chitkara.edu.in>
2022-07-09 08:37:38 +0530

    all pdf added app cleaned

--- client/src/components/assets/store/notesCardData.js
+++ client/src/components/assets/store/notesCardData.js
@@ -1,38 +1,17 @@
 // import notes_img1 from "./../images/notesImage/notes img1.svg";
-// import notes_img2 from "./../images/notesImage/notes img2.svg";
-// import notes_img3 from "./../images/notesImage/notes img3.svg";
+import notes_img2 from "./../images/notesImage/notes img2.svg";
+import notes_img3 from "./../images/notesImage/notes img3.svg";
 import notes_img4 from "./../images/notesImage/notes img4.svg";
 import notes_img5 from "./../images/notesImage/notes img5.svg";
-// import notes_img6 from "./../images/notesImage/notes img6.svg";
+import notes_img6 from "./../images/notesImage/notes img6.svg";
 import notes_img7 from "./../images/notesImage/notes img7.svg";
 import notes_img8 from "./../images/notesImage/notes img8.svg";
 import notes_img9 from "./../images/notesImage/notes img9.svg";
-// import notes_img10 from "./../images/notesImage/notes img10.svg";
-// import notes_img11 from "./../images/notesImage/notes img11.svg";
+import notes_img10 from "./../images/notesImage/notes img10.svg";
+import notes_img11 from "./../images/notesImage/notes img11.svg";
 
 
 const notesCardData = [
-    // {
-    //   id: 1,
-    //   title: 'Computer Networks',
-    //   category: 'CN',
-    //   img:notes_img10,
-    //   desc: `A computer network, also referred to as a data network, is a series of interconnected nodes that can transmit, receive and exchange data, voice and video traffic.`,
-    // },
-    // {
-    //   id: 2,
-    //   title: 'Design Analysis And Algorithm',
-    //   category: 'DAA',
-    //   img:notes_img2,
-    //   desc: `A finite set of instruction that specifies a sequence of operation is to be carried out in order to solve a specific problem or class of problems is called an Algorithm. `,
-    // },
-    // {
-    //   id: 3,
-    //   title: 'Operating System',
-    //   category: 'OS',
-    //   img:notes_img3,
-    //   desc: `An operating system (OS) is system software that manages computer hardware, software resources, and provides common services for computer programs.`,
-    // },
     {
       id: 1,
       title: 'Java',
@@ -47,13 +26,6 @@ const notesCardData = [
       img:notes_img5,
       desc: `Python is a general-purpose interpreted, interactive, object-oriented, and high-level programming language. It was created by Guido van Rossum during 1985- 1990.`,
     },
-    // {
-    //   id: 6,
-    //   title: 'C++',
-    //   category: 'Programming Language',
-    //   img:notes_img6,
-    //   desc: `C++ is an object oriented language and some concepts may be new. Take breaks when needed, and go over the examples as many times as needed.`,
-    // },
     {
       id: 3,
       title: 'Artifical Intelligence',
@@ -74,6 +46,43 @@ const notesCardData = [
       category: 'Programming Language',
       img:notes_img9,
       desc: `Object-oriented programming (OOP) is a style of programming characterized by the identification of classes of objects closely linked with the methods (functions) with which they are associated.`,
+    },
+    {
+      id: 6,
+      title: 'Computer Networks',
+      category: 'CN',
+      img:notes_img10,
+      desc: `(Apna College, Aman Dhattarwal) A computer network, also referred to as a data network, is a series of interconnected nodes that can transmit, receive and exchange data, voice and video traffic.`,
+    },
+    {
+      id: 7,
+      title: 'Dbms/Sql',
+      category: 'DBMS',
+      img:notes_img11,
+      desc: `(Apna College, Aman Dhattarwal) A database is a collection of related data which represents some aspect of the real
+      world.`,
+    },
+    {
+      id: 8,
+      title: 'Operating System',
+      category: 'OS',
+      img:notes_img6,
+      desc: `(Apna College, Aman Dhattarwal) An Operating System can be defined as an interface between user and hardware`,
+    },
+    {
+      id: 9,
+      title: 'Operating System',
+      category: 'OS',
+      img:notes_img3,
+      desc: `(Love Babbar) An operating system is a piece of software that manages all the resources of a computer
+      system, both	 hardware	 and	 software,	 and	 provides	 an	 environment`,
+    },
+    {
+      id: 10,
+      title: 'Python',
+      category: 'LANGUAGE',
+      img:notes_img2,
+      desc: `(CodeWithHarry hand written notes) Python is a simple and easy-to-understand language that feels like reading simple English. `,
     },
   ];
   export default notesCardData;
--- client/src/components/atoms/notesCardEach/index.js
+++ client/src/components/atoms/notesCardEach/index.js
@@ -1,12 +1,13 @@
 import React, {useState } from "react";
 import './index.css';
 import download_icon from './../../assets/icon/download.svg';
-// import pdf1 from './../../assets/pdfs/pdf_subid_1.pdf';
-// import pdf2 from './../../assets/pdfs/pdf_subid_2.pdf';
-// import pdf3 from './../../assets/pdfs/pdf_subid_3.pdf';
+import OsLoveBabbar from './../../assets/pdfs/OperatingSystem_loveBabbar.pdf';
+import Os from './../../assets/pdfs/OperatingSystem_ApnaCollege.pdf';
+import Dbms from './../../assets/pdfs/DBMS_ApnaCollege.pdf';
 import Java from './../../assets/pdfs/JAVA_dsacamp.pdf';
 import Python from './../../assets/pdfs/PYTHON_dsacamp.pdf';
-// import pdf6 from './../../assets/pdfs/pdf_subid_6.pdf';
+import PythonCodeWithHarry from './../../assets/pdfs/Python_CodeWithHarry.pdf';
+import Cn from './../../assets/pdfs/ComputerNetworks_ApnaCollege.pdf';
 import Ai from './../../assets/pdfs/AI_dsacamp.pdf';
 import Ml from './../../assets/pdfs/ML_dsacamp.pdf';
 import Oops from './../../assets/pdfs/OOPS_dsacamp.pdf';
@@ -21,7 +22,7 @@ const notesCardEach = ({ items }) => {
         const { id, title, img, desc } = item;
         function handlePdfView(x)
         {
-          var li=[Java,Python,Ai,Ml,Oops];
+          var li=[Java,Python,Ai,Ml,Oops,Cn,Dbms,Os,OsLoveBabbar,PythonCodeWithHarry];
           setView(li[x-1]);
         }
         return (
@@ -32,7 +33,7 @@ const notesCardEach = ({ items }) => {
                 <p className="notes__card__text">{desc}</p>
                 <div className="notes__card__button">
                   <a href={view} target="_black" className="notes__card__download__button" onClick={()=>{handlePdfView(id)}}>
-                    <img src={download_icon} className="notes__download" alt=""/>{'\u00A0'}Download PDF
+                    <img src={download_icon} className="notes__download" alt=""/>{'\u00A0'} PDF
                   </a>
                 </div>
               </div>
