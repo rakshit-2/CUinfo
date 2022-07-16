484659309fd7ac001716950b53a156ab47c3a20d
rakshit4013 <rakshit1063@chitkara.edu.in>
2022-07-16 09:15:30 +0530

    script added box shadow chnaged PA pdf added

--- client/src/components/assets/store/notesCardData.js
+++ client/src/components/assets/store/notesCardData.js
@@ -9,53 +9,61 @@ import notes_img8 from "./../images/notesImage/notes img8.svg";
 import notes_img9 from "./../images/notesImage/notes img9.svg";
 import notes_img10 from "./../images/notesImage/notes img10.svg";
 import notes_img11 from "./../images/notesImage/notes img11.svg";
+import notes_img12 from "./../images/notesImage/notes img12.svg";
 
 
 const notesCardData = [
     {
       id: 1,
+      title: 'Programming Abstraction',
+      category: 'Programming Language',
+      img:notes_img12,
+      desc: `This course is the natural successor to Programming Methodology and covers such advanced programming topics as recursion, etc.`,
+    },
+    {
+      id: 2,
       title: 'Java',
       category: 'Programming Language',
       img:notes_img4,
       desc: `Java is a programming language and a platform. Java is a high level, robust, object-oriented and secure programming language.`,
     },
     {
-      id: 2,
+      id: 3,
       title: 'Python',
       category: 'Programming Language',
       img:notes_img5,
       desc: `Python is a general-purpose interpreted, interactive, object-oriented, and high-level programming language. It was created by Guido van Rossum during 1985- 1990.`,
     },
     {
-      id: 3,
+      id: 4,
       title: 'Artifical Intelligence',
       category: 'AI',
       img:notes_img7,
       desc: `Artificial intelligence (AI) refers to the simulation of human intelligence in machines that are programmed to think like humans and mimic their actions.`,
     },
     {
-      id: 4,
+      id: 5,
       title: 'Machine Learning',
       category: 'ML',
       img:notes_img8,
       desc: `Machine learning is a branch of artificial intelligence (AI) and computer science which focuses on the use of data and algorithms to imitate the way that humans learn, gradually improving its accuracy.`,
     },
     {
-      id: 5,
+      id: 6,
       title: 'OOPS',
       category: 'Programming Language',
       img:notes_img9,
       desc: `Object-oriented programming (OOP) is a style of programming characterized by the identification of classes of objects closely linked with the methods (functions) with which they are associated.`,
     },
     {
-      id: 6,
+      id: 7,
       title: 'Computer Networks',
       category: 'CN',
       img:notes_img10,
       desc: `(Apna College, Aman Dhattarwal) A computer network, also referred to as a data network, is a series of interconnected nodes that can transmit, receive and exchange data, voice and video traffic.`,
     },
     {
-      id: 7,
+      id: 8,
       title: 'Dbms/Sql',
       category: 'DBMS',
       img:notes_img11,
@@ -63,14 +71,14 @@ const notesCardData = [
       world.`,
     },
     {
-      id: 8,
+      id: 9,
       title: 'Operating System',
       category: 'OS',
       img:notes_img6,
       desc: `(Apna College, Aman Dhattarwal) An Operating System can be defined as an interface between user and hardware`,
     },
     {
-      id: 9,
+      id: 10,
       title: 'Operating System',
       category: 'OS',
       img:notes_img3,
@@ -78,7 +86,7 @@ const notesCardData = [
       system, both	 hardware	 and	 software,	 and	 provides	 an	 environment`,
     },
     {
-      id: 10,
+      id: 11,
       title: 'Python',
       category: 'LANGUAGE',
       img:notes_img2,
